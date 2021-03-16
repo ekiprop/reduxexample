6478e98b4500660d5d62556cc4a5c0770c94468a
Kiprop <evanskiprop1@gmail.com>
2021-03-16 15:23:46 +0300

    redux boilerplate setup

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
 class Posts extends Component {
-  state = {
-    posts: [],
-  };
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
-  }
+  state = {};
 
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
-    const postItems = this.state.posts.map((post) => (
+    const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <h3>{post.body}</h3>
@@ -26,4 +25,12 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
+const mapStateToProps = (state) => ({
+  posts: state.posts.items,
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
